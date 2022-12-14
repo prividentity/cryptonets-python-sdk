a6e91d6aef78701ba597ea61f86e7708be37c88a
srieraam <srieraammohan@gmail.com>
2022-12-14 20:51:36 +0530

    doc: Sample code for each operation

---
+++ samples/sample_predict.py
@@ -0,0 +1,23 @@
+from cryptonets_python_sdk.factor import FaceFactor
+
+# Initialize API Key and Server URL
+api_key = "your_api_key"
+server_url = "provided_server_url"
+
+# Image path of the face
+image_file_path = "path_to_the_image"
+
+# Initialize the Face Factor object
+face_factor = FaceFactor(api_key=api_key, server_url=server_url)
+
+# Call predict from the face factor object
+predict_handle = face_factor.predict(image_path=image_file_path)
+
+# Parse enroll result
+# See https://docs.private.id/cryptonets-python-sdk/ResultObjects/EnrollPredictResult.html for detailed information
+print("Status:{}\nMessage:{}\nEnroll Level:{}\nUUID:{}\nGUID:{}\nToken:{}\n".format(predict_handle.status,
+                                                                                    predict_handle.message,
+                                                                                    predict_handle.enroll_level,
+                                                                                    predict_handle.uuid,
+                                                                                    predict_handle.guid,
+                                                                                    predict_handle.token))
