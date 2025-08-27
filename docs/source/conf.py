e066f66e1fdfa499a4fd589e34b8c1868f7fb6a0
Moez Bouselmi <moez.bouselmi@gmail.com>
2025-08-27 11:18:32 +0200

    Bump version to 1.3.21

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,7 +32,7 @@ copyright = "2025, Private Identity LLC"
 author = "Private Identity"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.20"
+release = "1.3.21"
 
 # -- General configuration ---------------------------------------------------
 
